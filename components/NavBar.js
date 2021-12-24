159548a0760aa76f1f4f1774c34e97f9f8b32450
Jin Park <jinpark1504@gmail.com>
2021-12-24 09:47:32 -0500

    navbar style

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,20 +1,31 @@
 import Link from 'next/link';
+import Image from 'next/image';
 import { useRouter } from 'next/router';
 
 export default function NavBar() {
   const router = useRouter();
   return (
     <nav>
-      <Link href='/'>
-        <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
-      </Link>
-      <Link href='/about'>
-        <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
-      </Link>
+      <Image src='/vercel.svg' alt='' width='142' height='32' />
+      <div>
+        <Link href='/'>
+          <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
+        </Link>
+        <Link href='/about'>
+          <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
+        </Link>
+      </div>
       <style jsx>{`
+        nav {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          gap: 10px;
+          padding: 1rem;
+          box-shadow: rgba(0, 0, 0, 0.3) 0px 30px 30px -24px;
+        }
         a {
           text-transform: uppercase;
-          margin-right: 0.5rem;
         }
         .active {
           color: tomato;
